4f7be9f0a4a1f214e12e0d9e0989b5f6437c74de
chandanaer <chandanaer9@gmail.com>
2026-07-06 00:21:51 +0530

    CalculatorTest.java

--- Exercise1/CalculatorTest.java
+++ Exercise1/CalculatorTest.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
     @Test
-    public void testAdd() {
+    void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
     }
